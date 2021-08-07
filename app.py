8bc986ad102e3f588f1d0c2cf37e452fbfe94372
chatbot-accn <88087640+chatbot-accn@users.noreply.github.com>
2021-08-07 11:57:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -262,7 +262,7 @@ def webhook():
         else:
             response = {
                 # 'fulfillmentText': total_value ,
-                'fulfillmentText': 'Click on the link provided here: http://127.0.0.1:5000/plots',
+                'fulfillmentText': 'Click on the link provided here: https://chatbot-accn.herokuapp.com/plots',
 
             }
             wb = data_mdfd
